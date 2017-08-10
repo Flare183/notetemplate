58b2dd5f07c07c8ada047ae501625b41c13e2af0
Unknown <Co2Noss@gmail.com>
2017-08-10 02:16:30 -0400

    3.022
    Analytics Update

--- nt.php
+++ nt.php
@@ -336,19 +336,9 @@ if(!$fgmembersite->CheckLogin())
                 }
             }
         </style>
-        <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-104085079-1', 'auto');
-
-        ga('send', 'pageview');
-
-        </script>
     </head>
 <body onbeforeunload="return confirm('Are you sure you want to close this ');" class="theme-background">
+    <?php include_once("analyticstracking.php") ?>
     <div id="content">
         <div id="panel">
         <h1>Note Template</h1>
